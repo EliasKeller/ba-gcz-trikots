accec17f01f343da46829fb9892c1bdef1c6117b
EliasKeller <elias.keller@gmail.com>
2026-04-21 17:53:42 +0200

    feat: league model

--- trikots/admin.py
+++ trikots/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from trikots.models import Country, Club
+from trikots.models import Country, Club, League
 
 admin.site.register(Country)
 admin.site.register(Club)
+admin.site.register(League)
