de06fd6f216c06f60231c930c1b967c742e48344
EliasKeller <elias.keller@gmail.com>
2026-04-21 17:48:17 +0200

    feat: club model

--- trikots/admin.py
+++ trikots/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from trikots.models import Country
+from trikots.models import Country, Club
 
 admin.site.register(Country)
+admin.site.register(Club)
--- trikots/models.py
+++ trikots/models.py
@@ -8,3 +8,12 @@ class Country(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Club(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100, unique=True)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
