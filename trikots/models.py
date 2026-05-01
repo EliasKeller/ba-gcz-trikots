54a43812d86f08d76618acd3ffba9665091b701c
EliasKeller <elias.keller@gmail.com>
2026-05-02 00:02:35 +0200

    feat: season

--- trikots/models.py
+++ trikots/models.py
@@ -25,3 +25,12 @@ class League(models.Model):
 
     def __str__(self):
         return self.name
+
+class Season(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100, unique=True)
+    startYear = models.IntegerField()
+    endYear = models.IntegerField()
+
+    def __str__(self):
+        return self.name
