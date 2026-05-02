a8534aa98226b57850577ee99d6439c8450724b3
EliasKeller <elias.keller@gmail.com>
2026-05-02 23:54:21 +0200

    ref: unused imports

--- trikots/models.py
+++ trikots/models.py
@@ -1,8 +1,6 @@
 import uuid
-from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
-from datetime import datetime
 
 from trikots.validators import max_year, validate_year_range
 
