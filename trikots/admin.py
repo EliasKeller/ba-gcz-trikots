8c871a0a9612fbde0755bdc10753a210fae0d3e3
EliasKeller <elias.keller@gmail.com>
2026-05-06 15:37:57 +0200

    feat: improve overview model detail view

--- trikots/admin.py
+++ trikots/admin.py
@@ -3,15 +3,57 @@ from django.utils.html import format_html
 
 from trikots.models import Country, Club, League, Season, Person, Supplier, SeasonClub, Match, Shirt
 
-admin.site.register(Country)
-admin.site.register(Club)
+#admin.site.register(Country)
+#admin.site.register(Club)
 admin.site.register(League)
 admin.site.register(Season)
 admin.site.register(Person)
 admin.site.register(Supplier)
 admin.site.register(SeasonClub)
-#admin.site.register(Match)
 admin.site.register(Shirt)
+
+MAX_LIST_SIZE = 25
+SEARCH_FIELD_PREFIX_PLACEHOLDER = "Such nach "
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+
+    list_display = [
+        "name",
+    ]
+
+    search_fields = [
+        "name"
+    ]
+
+    list_filter = [
+        "name",
+    ]
+    ordering = ["name"]
+
+    list_per_page = MAX_LIST_SIZE
+
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+
+    list_display = [
+        "name",
+        "country"
+    ]
+
+    search_fields = [
+        "name"
+    ]
+
+    list_filter = [
+        "name",
+        "country__name",
+    ]
+
+    ordering = ["name"]
+
+    search_help_text = SEARCH_FIELD_PREFIX_PLACEHOLDER + ",".join(search_fields)
+    list_per_page = MAX_LIST_SIZE
 
 @admin.register(Match)
 class MatchAdmin(admin.ModelAdmin):
@@ -38,7 +80,8 @@ class MatchAdmin(admin.ModelAdmin):
 
     list_select_related = ["home_club", "away_club"]
 
-    list_per_page = 25
+    list_per_page = MAX_LIST_SIZE
+    search_help_text = SEARCH_FIELD_PREFIX_PLACEHOLDER + ", ".join(search_fields)
 
     def result_colored(self, obj):
         return format_html(
