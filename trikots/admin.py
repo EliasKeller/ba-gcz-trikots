1c6304586c9418178f45fc16b9885da9a36a4d18
EliasKeller <elias.keller@gmail.com>
2026-05-06 14:40:24 +0200

    fix: styling of filtering in overview view of entity

--- trikots/admin.py
+++ trikots/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from trikots.models import Country, Club, League, Season, Person, Supplier, SeasonClub, Match, Shirt
 
@@ -14,5 +15,36 @@ admin.site.register(Shirt)
 
 @admin.register(Match)
 class MatchAdmin(admin.ModelAdmin):
-    search_fields = ["home_club__name", "away_club__name"]
-    list_filter = ["date"]
+
+    list_display = [
+        "date",
+        "home_club",
+        "away_club",
+        "result_colored",
+    ]
+
+    search_fields = [
+        "home_club__name",
+        "away_club__name",
+    ]
+
+    list_filter = [
+        "date",
+        "home_club",
+        "away_club",
+    ]
+
+    ordering = ["-date"]
+
+    list_select_related = ["home_club", "away_club"]
+
+    list_per_page = 25
+
+    def result_colored(self, obj):
+        return format_html(
+            "<strong>{}:{}</strong>",
+            obj.goals_home,
+            obj.goals_away
+        )
+
+    result_colored.short_description = "Resultat"
