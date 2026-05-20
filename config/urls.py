07fac307c2aa83a31c1f3f66227eb9a8e3ea5f16
EliasKeller <elias.keller@gmail.com>
2026-05-20 16:00:11 +0400

    feat: redirect to /admin if url is empty

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path
 
 from trikots.admin import custom_admin_dashboard
@@ -6,4 +7,5 @@ from trikots.admin import custom_admin_dashboard
 urlpatterns = [
     path("admin/", custom_admin_dashboard),
     path("admin/", admin.site.urls),
+    path('', lambda request: redirect('/admin/')),
 ]
